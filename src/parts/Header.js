b98d9d58fd216a09e97c3dbc15c8f5cb5e0bc3cc
devduduy <yudhapermana166@gmail.com>
2020-05-15 01:50:38 +0700

    Create Header component and put to arrange LandingPage

---
+++ src/parts/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Button from "elements/Button";
+import BrandIcon from "parts/IconText";
+
+const Header = (props) => {
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? "active" : "";
+  };
+
+  return (
+    <header className="spacing-sm">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <BrandIcon />
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className={`nav-item ${getNavLinkClass("/")}`}>
+                <Button className="nav-link" href="/" type="link">
+                  Home
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass("/browse-by")}`}>
+                <Button className="nav-link" href="/browse-by" type="link">
+                  Browse by
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass("/stories")}`}>
+                <Button className="nav-link" href="/stories" type="link">
+                  Stories
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass("/agents")}`}>
+                <Button className="nav-link" href="/agents" type="link">
+                  Agents
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
